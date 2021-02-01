23e12849918075de8945e866e797d611645a35bd
sm0862512 <Sm0862512@otc.edu>
2021-02-01 18:17:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+Hits = 0
+AtBats = 0
+BatAvg = 0
+
+
+Flag = bool(False)
+  if (Flag)
+
+AtBats = int(input("How many bats? " ))
+Hits = int(input("How many hits? " ))
+BatAvg = Hits / AtBats
+
+print("your batavg is" ,(BatAvg))
